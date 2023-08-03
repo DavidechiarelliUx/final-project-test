83315c31ea69d37690f9d782147c576c74ab8a71
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-08-04 00:12:21 +0200

    feat:Added view mode selection feature and implemented data grouping by departure port

--- src/components/cardList/cardList.jsx
+++ src/components/cardList/cardList.jsx
@@ -29,36 +29,44 @@
 
 // export default CardList;
 
+// step 4
 import React, {useState} from "react";
 import Card from "@/components/Card";
 import { mockData } from "@/mocks/mock";
 import styles from "./cardList.module.scss";
 
+const ViewModes = {
+    ShowAll: "Show All", 
+    ShowByPort: "Show By Port",
+};
 
-    const ViewModes= {
-        ShowAll: "Show All", 
-        ShowByPort: "Show By Port",
+const CardList = () => {
+    const [viewMode, setViewMode] = useState(ViewModes.ShowAll);
+    let groupedData;
+    if (viewMode === ViewModes.ShowByPort) {
+        groupedData = mockData.reduce((grouped, data) => {
+            (grouped[data.departure.Port] = grouped[data.departure.Port] || []).push(data);
+            return grouped;
+        }, {});
+        console.log(groupedData);
+    }
+    
+    const handleViewModeChange = (e) =>{
+        setViewMode(e.target.value);
     };
-
-    const CardList = () => {
-        const [ViewMode, SetViewMode] = useState(ViewModes.ShowAll);
-        
-        const handleViewModeChange = (e) =>{
-            SetViewMode(e.target.value);
-        };
 
     return(
         <>
         <div className={styles.container}>
-        <select className={styles.ShowBox} onChange={handleViewModeChange}>
-            <option value={ViewMode.ShowAll}>Mostra tutti</option>
-            <option value={ViewMode.ShowByPort}>Mostra per porto di partenza</option>
-        </select>
+            <select className={styles.ShowBox} onChange={handleViewModeChange}>
+                <option value={ViewModes.ShowAll}>Mostra tutti</option>
+                <option value={ViewModes.ShowByPort}>Mostra per porto di partenza</option>
+            </select>
         </div>
         <div> ciao </div>
         </>
     );
-
 }
+
 export default CardList;
 
