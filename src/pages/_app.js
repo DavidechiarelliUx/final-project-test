e5649fb050df496e479a52436f8ae7ed69323a8e
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-08-03 16:38:30 +0200

    feat: add helpers, typography, varia bles, in all.scss and add in global.scss

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+
