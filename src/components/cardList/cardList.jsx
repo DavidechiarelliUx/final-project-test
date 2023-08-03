bc3902d34280ecc8f8328882d082474689d7570d
DavidechiarelliUx <chiarellidavide99@gmail.com>
2023-08-03 21:55:14 +0200

    feat:add the show more button and completed step 2

--- src/components/cardList/cardList.jsx
+++ src/components/cardList/cardList.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
-import Card from '@/components/Card';
-import { mockData } from '@/mocks/mock';
-import styles from './cardList.module.scss';
+import React, {useState} from "react";
+import Card from "@/components/Card";
+import { mockData } from "@/mocks/mock";
+import styles from "./cardList.module.scss";
 
 const CardList = () => {
+
+    const [numCardsToShow, setNumCardsToShow] = useState(8);
+
+    
+    const handleShowMoreClick = () => {
+        setNumCardsToShow(numCardsToShow + 8);  
+    };
+
     return (
+        <>
         <div className={styles.cardList}>
-            {mockData.map((data) => (
+            {mockData.slice(0, numCardsToShow).map((data) => (
                 <Card key={data.id} data={data} /> 
-            ))}
+                ))}
         </div>
+            <div className={styles.containerButton}>
+                <button className={styles.button} onClick={handleShowMoreClick}>Mostra Altro</button>
+            </div>
+        </>
     );
 };
 
